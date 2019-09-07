84b1fb27d5dcaf45a96ca199eb72bdeca5c33452
Alex Labbane <alex.labbane@tamu.edu>
2019-09-07 15:24:53 -0500

    'calendar'

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import list from '@fullcalendar/list'
 
 @Component({
   selector: 'app-calendar',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  calendarPlugins = [dayGridPlugin, list]; // important!
 
   constructor() { }
 
