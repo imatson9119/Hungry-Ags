daadb2735df78964ef7b32c046c58d8dfc7a2561
Ian Matson <imatson9119@gmail.com>
2019-09-07 14:23:44 -0500

    fixed comma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FullCalendarModule } from "@fullcalendar/angular"; // for FullCalendar!
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
-import { PageHeadComponent } from './page-head/page-head.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { LoginComponent } from "./login/login.component";
+import { AboutComponent } from "./about/about.component";
+import { PageHeadComponent } from "./page-head/page-head.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
     LoginComponent,
-    AboutComponent
+    AboutComponent,
     PageHeadComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FullCalendarModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FullCalendarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
