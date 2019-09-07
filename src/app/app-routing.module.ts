9a109f0d5ebb66d8fc48edf6cc7eaaa5ebb298fb
Ian Matson <imatson9119@gmail.com>
2019-09-07 15:21:07 -0500

    fixed router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CalendarComponent } from './calendar/calendar.component'
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { LoginComponent } from "./login/login.component";
 
-import { AboutComponent } from './about/about.component'
-
+import { AboutComponent } from "./about/about.component";
 
 const routes: Routes = [
-{path:'', redirectTo : '/home', pathMatch : 'full'},  
-{path : 'home', component : CalendarComponent},
-{path : 'login', component : LoginComponent },
-{path : 'about', component : AboutComponent}];
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: CalendarComponent },
+  { path: "login", component: LoginComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
