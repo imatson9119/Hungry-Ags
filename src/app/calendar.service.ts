464f953d96bf225d446a77904ea15cd8f2269383
Alex Labbane <alex.labbane@tamu.edu>
2019-09-07 15:44:25 -0500

    'calendar_service'

--- src/app/calendar.service.ts
+++ src/app/calendar.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
 
-  get
+  getEvents() {
+      console.log(this.httpService.get('./calendar/events.json'));
+      return this.httpService.get('./calendar/events.json');
+  }
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import list from '@fullcalendar/list'
+import { CalendarService } from '../calendar.service'
 
 @Component({
   selector: 'app-calendar',
@@ -9,10 +10,12 @@ import list from '@fullcalendar/list'
 })
 export class CalendarComponent implements OnInit {
   calendarPlugins = [dayGridPlugin, list]; // important!
+  public events;
 
-  constructor() { }
+  constructor(private calendarService : CalendarService) { }
 
   ngOnInit() {
+    this.calendarService.getEvents().subscribe(events => this.events = events);
   }
 
 }
