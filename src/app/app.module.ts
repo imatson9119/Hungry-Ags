dd9e439debd819f8d6257547b438bce979a23031
Ian Matson <imatson9119@gmail.com>
2019-09-07 14:44:19 -0500

    Changed app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FullCalendarModule } from "@fullcalendar/angular"; // for FullCalendar!
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CalendarComponent } from "./calendar/calendar.component";
 import { LoginComponent } from "./login/login.component";
 import { AboutComponent } from "./about/about.component";
 import { PageHeadComponent } from "./page-head/page-head.component";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,13 @@ import { PageHeadComponent } from "./page-head/page-head.component";
     AboutComponent,
     PageHeadComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FullCalendarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FullCalendarModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
