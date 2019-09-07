026e6613ea885b73654f38461a15ed20f079d96b
Tanner Hoke <hoke-t@users.noreply.github.com>
2019-09-07 14:22:37 -0500

    father cleaned up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,18 @@ import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CalendarComponent } from './calendar/calendar.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
-=======
 import { PageHeadComponent } from './page-head/page-head.component';
->>>>>>> 1bbdec7547e9d379ed6a922dde3dcda5406fd0d7
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     CalendarComponent,
     LoginComponent,
     AboutComponent
-=======
     PageHeadComponent
->>>>>>> 1bbdec7547e9d379ed6a922dde3dcda5406fd0d7
   ],
   imports: [
     BrowserModule,
