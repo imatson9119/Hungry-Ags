9e09b5c25c253406d60ca77706c97d79dbf571e6
Tanner Hoke <hoke-t@users.noreply.github.com>
2019-09-08 02:53:18 -0500

    good footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  relative: boolean;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() { console.log(this.relative); }
+
+  getPosition() {
+    if (this.relative) {
+      return "relative";
+    } else {
+      return "absolute";
+    }
   }
-
 }
