d867405d130bb1a2f5dbc213f170b2f3a3f3639b
Ian Matson <imatson9119@gmail.com>
2019-09-07 20:32:24 -0500

    fixed router dynamics

--- src/app/page-head/page-head.component.ts
+++ src/app/page-head/page-head.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-page-head',
-  templateUrl: './page-head.component.html',
-  styleUrls: ['./page-head.component.scss']
+  selector: "app-page-head",
+  templateUrl: "./page-head.component.html",
+  styleUrls: ["./page-head.component.scss"]
 })
 export class PageHeadComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
