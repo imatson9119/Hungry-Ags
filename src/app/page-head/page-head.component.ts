034011b9ed70bcce6c3e83554a81375989944174
Alex Labbane <alex.labbane@tamu.edu>
2019-09-07 20:29:53 -0500

    'com'

--- src/app/page-head/page-head.component.ts
+++ src/app/page-head/page-head.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-head.component.scss']
 })
 export class PageHeadComponent implements OnInit {
+  public filter;
 
   constructor() { }
 
