334a2e1e03c36ff01d49b4092e77599a9b6eaf44
Alex Labbane <alex.labbane@tamu.edu>
2019-09-08 08:56:29 -0500

    test

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { CalendarService } from '../calendar.service';
+import { ControllerService } from '../shared/controller.service';
 
 @Component({
   selector: 'app-new-event',
@@ -8,14 +10,55 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewEventComponent implements OnInit {
   registrationForm: FormGroup;
-  constructor(public fb: FormBuilder) { }
-  
+  title : string;
+  location : string;
+  description : string;
+  start : string;
+  end : string;
+  organization : string;
+  user : string;
+
+  constructor(public fb: FormBuilder, public calendarService : CalendarService, public controller : ControllerService) { }
+
   ngOnInit() {
     this.registrationForm = this.fb.group({
       email: ['', [Validators.required,Validators.email]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
     });
+
+    this.calendarService.foodEvents.push({
+      eventName: "NEW",
+      user: this.controller.getStorage(this.controller.USERNAME_KEY),
+      sanctioned: false,
+      startTime: "2019-09-08T17:00:00+00:00",
+      endTime: "2019-09-08T18:00:00+00:00",
+      description: "",
+      location: "",
+      organization: "",
+      meetsCriteria : true
+    });
+
+    this.calendarService.calendarEvents.push({
+        title:this.title, 
+    });
+    console.log(this.calendarService.foodEvents[this.calendarService.foodEvents.length - 1].eventName);
+  }
+
+  addEvent() {
+    console.log("SERVED");
+    this.calendarService.foodEvents.push({
+      eventName: this.title,
+      user: "placeholder@tamu.edu",
+      sanctioned: false,
+      startTime: "2019-09-08T17:00:00+00:00",
+      endTime: "2019-09-08T18:00:00+00:00",
+      description: this.description,
+      location: this.location,
+      organization: this.organization,
+      meetsCriteria : true
+    });
+    console.log(this.calendarService.foodEvents[this.calendarService.foodEvents.length - 1].eventName);
   }
 
 }
