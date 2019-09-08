eb75366e2998b024780832382d0526bdfe7596bf
Ian Matson <imatson9119@gmail.com>
2019-09-07 19:42:06 -0500

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,18 @@ import { LoginComponent } from "./login/login.component";
 import { AboutComponent } from "./about/about.component";
 import { PageHeadComponent } from "./page-head/page-head.component";
 import { MatButtonModule } from "@angular/material/button";
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { RegisterComponent } from './register/register.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import {
+  RegisterComponent,
+  ConfirmEmailDialog
+} from "./register/register.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
@@ -27,7 +30,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     PageHeadComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    ConfirmEmailDialog
   ],
   imports: [
     BrowserModule,
@@ -40,9 +44,11 @@ import { FooterComponent } from './footer/footer.component';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [HttpClientModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ConfirmEmailDialog]
 })
 export class AppModule {}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
 
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  animal: string;
+  name: string;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+  ngOnInit() {}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmEmailDialog, {
+      width: "250px",
+      data: { name: this.name, animal: this.animal }
+    });
 
-  ngOnInit() {
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("The dialog was closed");
+      this.animal = result;
+    });
   }
+}
 
+@Component({
+  selector: "confirm-email",
+  templateUrl: "confirm-email.html"
+})
+export class ConfirmEmailDialog {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmEmailDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
