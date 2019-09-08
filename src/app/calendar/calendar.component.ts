dfb8b7cde1d11c72dc079c0be27173ea6ae02c24
Ian Matson <imatson9119@gmail.com>
2019-09-08 16:05:47 -0500

    attempted to fix calender msg

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,50 +1,65 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import listPlugin from '@fullcalendar/list'
-import { CalendarService } from '../calendar.service'
-import { FoodEvent } from './FoodEvent';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { Observable } from 'rxjs';
-import { TouchSequence } from 'selenium-webdriver';
-import { buildings } from '../MockMapExtensions'
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Component, OnInit, Inject } from "@angular/core";
+import listPlugin from "@fullcalendar/list";
+import { CalendarService } from "../calendar.service";
+import { FoodEvent } from "./FoodEvent";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { Observable } from "rxjs";
+import { TouchSequence } from "selenium-webdriver";
+import { buildings } from "../MockMapExtensions";
+import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
 
 export interface Data {
-    title : string;
-    location : string;
-    description : string;
-    organization : string;
-    doLink : boolean;
-    mapLink : string;
+  title: string;
+  location: string;
+  description: string;
+  organization: string;
+  doLink: boolean;
+  mapLink: string;
 }
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  selector: "app-calendar",
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.scss"]
 })
 export class CalendarComponent implements OnInit {
   calendarPlugins = [listPlugin]; // important!
-  public events : FoodEvent[];
+  public events: FoodEvent[];
   public calendarObjects: Object[] = [];
-  public doLink : boolean;
+  public doLink: boolean;
 
-  constructor(private calendarService : CalendarService, public dialog : MatDialog) { }
+  constructor(
+    private calendarService: CalendarService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    this.calendarService.getEvents().subscribe(events => this.events = events);
+    this.calendarService
+      .getEvents()
+      .subscribe(events => (this.events = events));
     console.log("Events Length", this.events.length);
     this.calendarObjects = [];
-    this.calendarService.getCalendarEvents().subscribe(events => this.calendarObjects = events);
+    this.calendarService
+      .getCalendarEvents()
+      .subscribe(events => (this.calendarObjects = events));
   }
 
-  
-
   openDialog(arg): void {
-    let event = {eventName:"", location:"",description:"",organization:""};
+    let event = {
+      eventName: "",
+      location: "",
+      description: "",
+      organization: ""
+    };
     for (let i = 0; i < this.events.length; i++) {
-      let verificationString = this.events[i].location + " - " + this.events[i].eventName;
+      let verificationString =
+        this.events[i].location + " - " + this.events[i].eventName;
       let selectedString = arg.event._def.title;
-      if(selectedString == verificationString) {
+      if (selectedString == verificationString) {
         event = this.events[i];
         console.log("EVENT FOUND");
       }
@@ -53,7 +68,7 @@ export class CalendarComponent implements OnInit {
     this.doLink = false;
     let mapLink = "https://aggiemap.tamu.edu/map/d?ref=HungryAgs&BldgAbbrv=";
     for (let i = 0; i < buildings.length; i++) {
-      if(event.location.indexOf(buildings[i]) != -1) {
+      if (event.location.indexOf(buildings[i]) != -1) {
         this.doLink = true;
         mapLink += buildings[i];
         console.log(buildings[i]);
@@ -61,46 +76,56 @@ export class CalendarComponent implements OnInit {
     }
 
     const dialogRef = this.dialog.open(EventDialog, {
-      width: '400px',
-      data: {title:event.eventName,
-        location:event.location, 
-        description:event.description,
-        organization:event.organization,
-        doLink:this.doLink,
-        mapLink:mapLink}
+      width: "400px",
+      data: {
+        title: event.eventName,
+        location: event.location,
+        description: event.description,
+        organization: event.organization,
+        doLink: this.doLink,
+        mapLink: mapLink
+      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+      console.log("The dialog was closed");
     });
+  }
+  changeHTML() {
+    console.log("Changing HTML...");
+    let test = document.getElementsByClassName("fc-list-empty").item(0);
+    if (test) {
+      console.log(test.attributes);
+      test.innerHTML =
+        '<i class="material-icons" style="color: rgba(0, 0, 0, 0.4); font-size: 150px;">inbox</i><br><div style="color: rgba(0, 0, 0, 0.4); font-size: 45px;">Nothing here!</div> ';
+    }
   }
 }
 
 @Component({
-  selector: 'dialog-overview-example-dialog',
-  templateUrl: 'eventDialog.html',
-  styleUrls:['eventDialog.scss']
+  selector: "dialog-overview-example-dialog",
+  templateUrl: "eventDialog.html",
+  styleUrls: ["eventDialog.scss"]
 })
 export class EventDialog {
-
   constructor(
     public dialogRef: MatDialogRef<EventDialog>,
     @Inject(MAT_DIALOG_DATA) public data: Data,
-    private _snackBar: MatSnackBar) {}
+    private _snackBar: MatSnackBar
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   reportClick() {
-    this._snackBar.open('Your report has been received.', 'Okay', {
+    this._snackBar.open("Your report has been received.", "Okay", {
       duration: 3000
     });
   }
 
-  showMap() : boolean {
-    if(this.data.doLink) return true;
+  showMap(): boolean {
+    if (this.data.doLink) return true;
     return false;
   }
-
 }
--- src/app/page-head/page-head.component.ts
+++ src/app/page-head/page-head.component.ts
@@ -1,10 +1,10 @@
-import { AppComponent } from '../app.component';
+import { AppComponent } from "../app.component";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { CalendarService } from '../calendar.service';
-import { stringify } from 'querystring';
-import { ControllerService } from '../shared/controller.service';
-import { getMaxListeners } from 'cluster';
+import { CalendarService } from "../calendar.service";
+import { stringify } from "querystring";
+import { ControllerService } from "../shared/controller.service";
+import { getMaxListeners } from "cluster";
 
 @Component({
   selector: "app-page-head",
@@ -12,72 +12,84 @@ import { getMaxListeners } from 'cluster';
   styleUrls: ["./page-head.component.scss"]
 })
 export class PageHeadComponent implements OnInit {
-  constructor(public controllerService: ControllerService, public router: Router, public calendarService:CalendarService) {}
-  public filter : string = "";
-  public wasHome : boolean = false;
+  constructor(
+    public controllerService: ControllerService,
+    public router: Router,
+    public calendarService: CalendarService
+  ) {}
+  public filter: string = "";
+  public wasHome: boolean = false;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   checkIsHome() {
     if (!this.wasHome) {
       this.filter = "";
     }
     this.calendarService.calendarEvents = [];
-    if(this.filter.length > 0) {
+    if (this.filter.length > 0) {
       this.calendarService.calendarEvents = [];
       let events = this.calendarService.foodEvents;
-      let filters = this.filter.split(' ');
-      for (let i = 0; i < events.length ; i++) {
-        let event = [events[i].description, 
-          events[i].eventName, 
-        events[i].location, 
-        events[i].organization];
+      let filters = this.filter.split(" ");
+      for (let i = 0; i < events.length; i++) {
+        let event = [
+          events[i].description,
+          events[i].eventName,
+          events[i].location,
+          events[i].organization
+        ];
         let numFound = 0;
         for (let j = 0; j < filters.length; j++) {
-          for(let k = 0; k < event.length; k++) {
-            console.log(filters[j],"vs",event[k])
-            if(event[k].toLowerCase().indexOf(filters[j].toLowerCase()) != -1){
+          for (let k = 0; k < event.length; k++) {
+            //console.log(filters[j],"vs",event[k])
+            if (
+              event[k].toLowerCase().indexOf(filters[j].toLowerCase()) != -1
+            ) {
               numFound++;
-              console.log("MATCH FOUND");
+              //console.log("MATCH FOUND");
               break;
             }
           }
-          console.log("Num found:",numFound,"vs expected",1+j);
-          if(numFound != 1 + j) break;
+          //console.log("Num found:",numFound,"vs expected",1+j);
+          if (numFound != 1 + j) break;
         }
 
-        if(numFound == filters.length) {
+        if (numFound == filters.length) {
           let color = "#6c1420";
-          if(!events[i].sanctioned) color = "#00000000";
-          this.calendarService.calendarEvents.push({title:events[i].location + " - " + events[i].eventName
-            , start:events[i].startTime
-            , end:events[i].endTime, color:color});
+          if (!events[i].sanctioned) color = "#00000000";
+          this.calendarService.calendarEvents.push({
+            title: events[i].location + " - " + events[i].eventName,
+            start: events[i].startTime,
+            end: events[i].endTime,
+            color: color
+          });
         }
       }
-      console.log("LENGTH:", this.calendarService.calendarEvents.length);
+      //console.log("LENGTH:", this.calendarService.calendarEvents.length);
     } else {
       let events = this.calendarService.foodEvents;
       for (let i = 0; i < events.length; i++) {
         let color = "#6c1420";
-        if(!events[i].sanctioned) color = "#00000000";
-        this.calendarService.calendarEvents.push({title:events[i].location + " - " + events[i].eventName
-          , start:events[i].startTime
-          , end:events[i].endTime, color:color});
+        if (!events[i].sanctioned) color = "#00000000";
+        this.calendarService.calendarEvents.push({
+          title: events[i].location + " - " + events[i].eventName,
+          start: events[i].startTime,
+          end: events[i].endTime,
+          color: color
+        });
       }
-      console.log("LENGTH:", this.calendarService.calendarEvents.length);
-
+      //console.log("LENGTH:", this.calendarService.calendarEvents.length);
     }
 
-    this.wasHome = this.router.url === '/home';
+    this.wasHome = this.router.url === "/home";
     return this.wasHome;
   }
-  getDisplayName(){
-    let tempName = this.controllerService.getStorage(this.controllerService.USERNAME_KEY);
-    if(tempName == null || !this.controllerService.signedIn)
-      return '';
-    if(tempName.length >= 25)
-      tempName = tempName.substr(0,22) + "...";
+  getDisplayName() {
+    let tempName = this.controllerService.getStorage(
+      this.controllerService.USERNAME_KEY
+    );
+    if (tempName == null || !this.controllerService.signedIn) return "";
+    if (tempName.length >= 25) tempName = tempName.substr(0, 22) + "...";
     return tempName;
   }
 }
